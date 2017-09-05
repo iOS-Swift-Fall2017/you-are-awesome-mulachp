a2ef1a14d9d0e75d9e4976a7d8b895a19848e836
Patrick <patrick@Patricks-MacBook-Pro-2.local>
2017-09-05 15:40:22 -0400

    commit

--- Week One in Class/ViewController.swift
+++ Week One in Class/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// put outlets here 
+var index = 0
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("*** viewDidLoad has exexuted", Date())
+       
+      
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+     let messages = ["You are fantastic",
+                     "You are great!" ,
+                     "You are amazing",
+                     "When the genius bar needs help, they call you!",
+                     "You Brighten My Day!"]
+      var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        } while index == newIndex
+        
+       index = newIndex
+        
+        messageLabel.text = messages[index]
+        
+   //     var randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+   //     messageLabel.text = messages[randomIndex]
+  /*
+        messageLabel.text = messages[index]
+        index = index+1
+        if index == messages.count - 1 {
+            index = 0}
+        else {index = index + 1
+        }
+ */
+ 
+        
+        
+ /* let message1 = "You are the Bomb!"
+ let message2 = "You are Great!"
+ let message3 = "You are Amazing!"if messageLabel.text == message1 {
+            messageLabel.text = message2
+        } else if messageLabel.text == message2 {
+        messageLabel.text = message3 }
+        else { messageLabel.text = message1
     }
-
-
+*/
 }
 
+}
